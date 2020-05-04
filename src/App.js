c063deeb150e5264600e798d3021989c4252cb76
sahilb8 <sahilbanastarkar7184@gmail.com>
2020-05-04 11:52:25 +0530

    added routing capability

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder';
+import Checkout from '../src/containers/Checkout/Checkout';
+import {Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          
+          </Switch>
         </Layout>
     </div>
   );
